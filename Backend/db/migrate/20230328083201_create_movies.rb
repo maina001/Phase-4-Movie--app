be701c94d4b94d3401b6a6ba35d375fc4ac8757f
GriffinsNgeno <117969990+GriffinsNgeno@users.noreply.github.com>
2023-03-28 11:46:52 +0300

    Created Movie table

--- Backend/db/migrate/20230328083201_create_movies.rb
+++ Backend/db/migrate/20230328083201_create_movies.rb
@@ -1,6 +1,9 @@
 class CreateMovies < ActiveRecord::Migration[7.0]
   def change
     create_table :movies do |t|
+      t.string :title
+      t.integer :year
+      t.string :director
 
       t.timestamps
     end
