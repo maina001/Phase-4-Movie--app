8088f3100b83841b979a341aa2f99b838bbc85c1
GriffinsNgeno <117969990+GriffinsNgeno@users.noreply.github.com>
2023-03-28 15:08:06 +0300

    Updated routes

--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :movies
+  resources :reviews, only: [:create, :destroy]
+  resources :movies, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
