e73499eb3250ee9db65ab7a679f8b132b7188803
GriffinsNgeno <117969990+GriffinsNgeno@users.noreply.github.com>
2023-03-29 09:43:49 +0300

    Added custom routes

--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
-  resources :reviews, only: [:create, :destroy]
-  resources :movies, only: [:index, :show]
+    # GET route for getting a review by id
+  get '/reviews/:id', to: 'reviews#show'
+
+    # GET route for getting all reviews
+  get '/reviews', to: 'reviews#index'
+
+    # DELETE route for deleting a review by id
+  delete '/reviews/:id', to: 'reviews#destroy'
+  
+    # GET route for getting all movies
+  get '/movies', to: 'movies#index'
+
+    # GET route for getting a movie by id
+  get '/movies/:id', to: 'movies#show'
+
+
+
+  # resources :movies, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
