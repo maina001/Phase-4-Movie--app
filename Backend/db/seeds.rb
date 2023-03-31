23f4becd264cf6054edc311f54640d7d313d3b6d
GriffinsNgeno <117969990+GriffinsNgeno@users.noreply.github.com>
2023-03-31 10:13:49 +0300

    Update seeds.rb

--- Backend/db/seeds.rb
+++ Backend/db/seeds.rb
@@ -1,6 +1,6 @@
 require 'faker'
 puts "Seeding..."
-# Generate fake movie data
+# Create 10 fake movie data
 10.times do
   Movie.create( 
     image: Faker::LoremFlickr.image(size: "300x300", search_terms: ['movie']),
@@ -11,7 +11,7 @@ puts "Seeding..."
   )
 end
 
-# Generate fake review data
+# Create 10 fake review data
 10.times do
   Review.create( 
     rating: Faker::Number.between(from: 1, to: 5),
@@ -20,5 +20,15 @@ end
     movie_id: Faker::Number.between(from: 1, to: 10)
   )
 end
+
+# Create 10 movie reviews
+10.times do
+  # Create a new movie review
+  movie_review = MovieReview.create(
+    movie_id: Faker::Number.between(from: 1, to: 100), # Random movie ID
+    review_id: Faker::Number.between(from: 1, to: 100), # Random review ID
+  )
+end
+
 
 puts "Done seeding"
